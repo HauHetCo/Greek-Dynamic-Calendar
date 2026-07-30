56236624b2899cea52d1450138ba5a2f689b2520
HauHet.Co <63187237+HauHetCo@users.noreply.github.com>
2026-07-31 00:29:16 +0300

    Add files via upload

---
+++ Calendar/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'db_host' => 'localhost:3306',
+    'db_name' => 'user4888135314_',
+    'db_user' => 'eortologio_db',
+    'db_pass' => 'tZ*ryYKymr~913uw',
+    'db_charset' => 'utf8mb4',
+    'timezone' => 'Europe/Athens',
+];

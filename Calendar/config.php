e393b0b1fb41fcaa0ddd4be197597373908f7587
HauHet.Co <63187237+HauHetCo@users.noreply.github.com>
2026-07-31 00:30:13 +0300

    Update config.php

--- Calendar/config.php
+++ Calendar/config.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'db_host' => 'localhost:3306',
-    'db_name' => 'user4888135314_',
+    'db_name' => 'root',
     'db_user' => 'eortologio_db',
-    'db_pass' => 'tZ*ryYKymr~913uw',
+    'db_pass' => '',
     'db_charset' => 'utf8mb4',
     'timezone' => 'Europe/Athens',
 ];
